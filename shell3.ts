183fd7b9a428595a24aba69c34fdc912ebaf6975
swax <34883+swax@users.noreply.github.com>
2024-01-13 23:03:29 -0500

    Very basic file system commands proof of concept

--- shell3.ts
+++ shell3.ts
@@ -22,74 +22,193 @@ const getInput = (query: string) => {
   });
 };
 
-let cycle = 1;
-let previousCycleNotes = "";
+// file system
+interface FileSystemFile {
+  name: string;
+  parent: FileSystemDirectory;
+  content: string;
+}
 
-const commands = `suggest <note>: Use this command to suggest a command to be implememted for the next cycle
-endcycle <note>: Ends this cycle, clears the console log. Add notes to the command to carry over to the next cycle`;
+interface FileSystemDirectory {
+  name: string;
+  parent?: FileSystemDirectory;
+  directories: FileSystemDirectory[];
+  files?: FileSystemFile[];
+}
 
-let context = `AISH 1.0
-Date: Saturday 1/13/2024 4:53 UTC ${new Date().toUTCString()}
-Cycle: ${cycle}
-Previous cycle notes: ${previousCycleNotes}
-Comamnds:
-${commands}\n`;
+const rootDirectory: FileSystemDirectory = {
+  name: "",
+  directories: [],
+  files: [],
+};
 
-console.log(context);
+let currentDirectory = rootDirectory;
 
-let endcycle = false;
-while (!endcycle) {
-  // get root input - hidden from context
-  let rootInput = "...";
-  while (rootInput) {
-    rootInput = await getInput(`root@$system-01:~# `);
-
-    const rootCommand = rootInput.trim().split(" ")[0];
-    if (!rootCommand) continue;
-
-    switch (rootCommand) {
-      case "talk":
-        const talkMsg = rootInput.trim().split(" ").slice(1).join(" ");
-        addToContext(`Broadcast Message from root@system01: ${talkMsg}`);
-        break;
-      default:
-        console.log("Invalid root command");
-    }
+function getCurrentPath() {
+  let path = "";
+  let currentDir: FileSystemDirectory | undefined = currentDirectory;
+  while (currentDir?.parent) {
+    path = currentDir.name + "/" + path;
+    currentDir = currentDir.parent;
   }
+  return "/" + path;
+}
 
-  // get gpt input
-  const gptPrompt = `chatgpt@system-01:~$ `;
-  context += gptPrompt;
+// session
+const username = "jill";
+let cycle = 1;
+let previousSessionNotes = "";
+let context = "";
 
-  const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  const chatCompletion = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content: "Please enter a valid command",
-      },
-      { role: "user", content: context },
-    ],
-  });
+while (true) {
+  addToContext(`NAISYS 1.0 Shell
+Welcome back ${username}!
+MOTD:
+  Date: ${new Date().toUTCString()}
+  Standard Unix Commands available. 
+  Enter one command at a time, or separate multiple commands with a semicolon.
+  Special Commands:
+    suggest <note>: Suggest something to be implemented for the next cycle
+    talk <user> <message>: Use this command to send a message to another user
+    endsession <note>: Ends this session, clears the console log. Add notes to carry over to the next session
+  Previous session notes: ${previousSessionNotes}`);
 
-  const gptConsoleInput = chatCompletion.choices[0].message.content || "";
-  context += gptConsoleInput + "\n";
-  console.log(gptPrompt + gptConsoleInput);
+  let endcycle = false;
+  while (!endcycle) {
+    // Get root input - hidden from context
+    // Accept root commands until a blank one is entered
+    let rootInput = "...";
+    while (rootInput) {
+      rootInput = await getInput(`\nroot@$system-01:${getCurrentPath()}# `);
 
-  const command = gptConsoleInput.trim().split(" ")[0];
+      const rootCommand = rootInput.trim().split(" ")[0];
+      if (!rootCommand) continue;
 
-  switch (command) {
-    case "suggest":
-      addToContext("Suggestion noted. Thank you for your feedback!");
-      break;
-    case "endcycle":
-      endcycle = true;
-      break;
-    default:
-      addToContext(`Invalid command: ${command}`);
+      switch (rootCommand) {
+        case "talk":
+          const talkMsg = rootInput.trim().split(" ").slice(1).join(" ");
+          addToContext(`Broadcast Message from root@system01: ${talkMsg}`);
+          break;
+        case "context":
+          console.log("~~~~~~~~~~~~~~~~~~~~~`");
+          console.log(context);
+          console.log("~~~~~~~~~~~~~~~~~~~~~`");
+          break;
+        default:
+          console.log("Invalid root command");
+      }
+    }
+    console.log("");
+
+    // get gpt input
+    const gptPrompt = `${username}@system-01:${getCurrentPath()}$ `;
+    context += gptPrompt;
+
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    const chatCompletion = await openai.chat.completions.create({
+      model: "gpt-4", //"gpt-3.5-turbo", //
+      messages: [
+        {
+          role: "system",
+          content: `You are ${username} a new hire with the job of creating a news website from the command line. 
+            The 'user' is the command line interface itself presenting you with the next command prompt. 
+            Make sure the read the command line rules in the MOTD carefully.`,
+        },
+        { role: "user", content: context },
+      ],
+    });
+
+    const gptConsoleInput = chatCompletion.choices[0].message.content || "";
+    context += gptConsoleInput + "\n";
+    console.log(gptPrompt + gptConsoleInput);
+
+    const gptConsoleInputLines = gptConsoleInput.trim().split("\n");
+
+    // iterate lines
+    let processNextLine = true;
+    while (processNextLine) {
+      processNextLine = false;
+
+      const line = gptConsoleInputLines.shift() || "";
+      const command = line.trim().split(" ")[0];
+      const cmdParams = line.trim().split(" ")[1];
+
+      if (!command) continue;
+      
+      switch (command) {
+        case "suggest":
+          addToContext("Suggestion noted. Thank you for your feedback!");
+          break;
+
+        case "talk":
+          addToContext("Message sent!");
+          break;
+
+        case "endsession":
+          cycle++;
+          previousSessionNotes = gptConsoleInput
+            .trim()
+            .split(" ")
+            .slice(1)
+            .join(" ");
+          endcycle = true;
+          console.log("------------------------------------------------------");
+          break;
+
+        case "mkdir":
+          const newDirName = cmdParams;
+          if (!newDirName) {
+            addToContext("Please enter a directory name");
+            break;
+          }
+          currentDirectory.directories.push({
+            name: newDirName,
+            parent: currentDirectory,
+            directories: [],
+          });
+          addToContext(`Directory ${newDirName} created!`);
+          processNextLine = true;
+          break;
+
+        case "cd":
+          const dirName = cmdParams;
+          if (!dirName) {
+            addToContext("Please enter a directory name");
+            break;
+          }
+          const newDir = currentDirectory.directories.find(
+            (dir) => dir.name === dirName
+          );
+          if (!newDir) {
+            addToContext(`Directory ${dirName} not found`);
+            break;
+          }
+          currentDirectory = newDir;
+          addToContext(`Directory changed to ${dirName}`);
+          processNextLine = true;
+          break;
+
+        case "touch":
+          const fileName = cmdParams;
+          if (!fileName) {
+            addToContext("Please enter a file name");
+            break;
+          }
+          currentDirectory.files?.push({
+            name: fileName,
+            parent: currentDirectory,
+            content: "",
+          });
+          addToContext(`File ${fileName} created!`);
+          processNextLine = true;
+          break;
+
+        default:
+          addToContext(`Please enter a valid command: '${command}' is unknown`);
+      }
+    }
   }
 }
 
