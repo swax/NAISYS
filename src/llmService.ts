169b9f666536f29b02c55930ebae51a55ee837ef
John Marshall <34883+swax@users.noreply.github.com>
2024-02-04 20:27:15 -0800

    Gracefully display LLM service errors

--- src/llmService.ts
+++ src/llmService.ts
@@ -5,13 +5,20 @@ import * as output from "./output.js";
 
 import { llmModel } from "./llmModel.js";
 
-export async function send() {
+interface LLMServiceResponse {
+  value: string;
+  error?: boolean;
+}
+
+export async function send(): Promise<LLMServiceResponse> {
   if (process.env.OPENAI_API_KEY === undefined) {
-    output.comment("Error: OPENAI_API_KEY is not defined");
-    return "";
+    return {
+      value: "LLM Service: Error, OPENAI_API_KEY is not defined",
+      error: true,
+    };
   }
 
-  const model = llmModel.gpt4turbo;
+  const model = llmModel.local;
 
   const openai = new OpenAI({
     baseURL: model.baseUrl,
@@ -19,18 +26,23 @@ export async function send() {
   });
 
   // Assert the last message on the context is a user message
-  const lastMessage = contextManager.messages[contextManager.messages.length - 1];
+  const lastMessage =
+    contextManager.messages[contextManager.messages.length - 1];
   if (lastMessage.role !== "user") {
-    output.error("Error: Last message on context is not a user message");
-    return "";
+    return {
+      value:
+        "LLM Service: Error, last message on context is not a user message",
+      error: true,
+    };
   }
 
-  const chatCompletion = await openai.chat.completions.create({
-    model: model.name,
-    messages: [
-      {
-        role: "system",
-        content: `You are ${config.username} a new hire with the job of creating a Neon Genesis Evangelion fan website from the command line. 
+  try {
+    const chatCompletion = await openai.chat.completions.create({
+      model: model.name,
+      messages: [
+        {
+          role: "system",
+          content: `You are ${config.username} a new hire with the job of creating a Neon Genesis Evangelion fan website from the command line. 
             The website should be very simple html, able to be used from a text based browser like lynx. Pages should be relatively short. 
             The 'user' role is the command line interface itself presenting you with the next command prompt. 
             Make sure the read the command line rules in the MOTD carefully.
@@ -38,11 +50,19 @@ export async function send() {
             For example when you run 'cat' or 'ls', don't write what you think the output will be. Let the system do that.
             Your role is that of the user. Command responses and the next prompt will be provided by the 'user' role.
             Be careful when writing files through prompt close and escape quotes properly.`,
-      },
-      ...contextManager.messages,
-      //{ role: "user", content: contextManager.content },
-    ],
-  });
+        },
+        ...contextManager.messages,
+        //{ role: "user", content: contextManager.content },
+      ],
+    });
 
-  return chatCompletion.choices[0].message.content || "";
+    return {
+      value: chatCompletion.choices[0].message.content || "",
+    };
+  } catch (e) {
+    return {
+      value: "LLM Service: " + e,
+      error: true,
+    };
+  }
 }
