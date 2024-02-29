7d80559e091bdb75f28a163f72b06ff74c899913
swax <34883+swax@users.noreply.github.com>
2024-02-29 12:39:33 -0800

    Prevent command chords from cancelling timeout of prompt when pressed Indicate in prompt that timeout has been disabled

--- src/command/promptBuilder.ts
+++ src/command/promptBuilder.ts
@@ -1,4 +1,5 @@
 import chalk from "chalk";
+import * as events from "events";
 import * as readline from "readline";
 import * as llmail from "../apps/llmail.js";
 import * as config from "../config.js";
@@ -8,7 +9,18 @@ import { InputMode } from "../utils/inputMode.js";
 import * as output from "../utils/output.js";
 import * as shellWrapper from "./shellWrapper.js";
 
-readline.emitKeypressEvents(process.stdin);
+// When actual output is entered by the user we want to cancel any auto-continue timers and/or wake on message
+// We don't want to cancel if the user is entering a chords like ctrl+b then down arrow, when using tmux
+// This is why we can't put the event listener on the standard process.stdin/keypress event.
+// There is no 'data entered' output event so this monkey patch does that
+const _writeEventName = "write";
+const _outputEmitter = new events.EventEmitter();
+const _originalWrite = process.stdout.write.bind(process.stdout);
+
+process.stdout.write = (...args) => {
+  _outputEmitter.emit(_writeEventName);
+  return _originalWrite.apply(process.stdout, <any>args);
+};
 
 const _readlineInterface = readline.createInterface({
   input: process.stdin,
@@ -55,7 +67,7 @@ export function getInput(commandPrompt: string, pauseSeconds?: number) {
     let timeout: NodeJS.Timeout;
     let interval: NodeJS.Timeout;
 
-    function cancelTimeouts() {
+    function cancelTimeouts(expired?: boolean) {
       if (timeout) {
         clearTimeout(timeout);
       }
@@ -63,23 +75,38 @@ export function getInput(commandPrompt: string, pauseSeconds?: number) {
         clearInterval(interval);
       }
 
-      process.stdin.off("keypress", cancelTimeouts);
-    }
+      _outputEmitter.off(_writeEventName, cancelTimeouts);
 
-    // If user starts typing in prompt, cancel any auto timeouts or wake on msg
-    process.stdin.on("keypress", cancelTimeouts);
+      // If timeout interrupted by user input, clear out the timeout information from the prompt
+      // to prevent the user from thinking the timeout still applies
+      // BUG: When the user hits delete, the prompt is reset to the original which is confusing as user will think timeout is still active
+      // Fix is probably to reset the entire the question when the timeout is interrupted
+      if (!expired) {
+        let pausePos = commandPrompt.indexOf("[Paused:");
+        pausePos = pausePos == -1 ? commandPrompt.indexOf("[WakeOnMsg]") : pausePos;
+        
+        if (pausePos > 0) {
+          const charsBack = commandPrompt.length - pausePos - 1; // pluse 1 for the space after the #
+          readline.moveCursor(process.stdout, -charsBack, 0);
+          process.stdout.write("-".repeat(charsBack - 3));
+          readline.moveCursor(process.stdout, 3, 0);
+        }
+      }
+    }
 
     _readlineInterface.question(
       chalk.greenBright(commandPrompt),
-      { signal: ac.signal },
+      { signal: ac.signal  },
       (answer) => {
-        cancelTimeouts();
         resolve(answer);
       },
     );
 
+    // If user starts typing in prompt, cancel any auto timeouts or wake on msg
+    _outputEmitter.on(_writeEventName, cancelTimeouts);
+
     const abortQuestion = () => {
-      cancelTimeouts();
+      cancelTimeouts(true);
       ac.abort();
       resolve("");
     };
@@ -87,7 +114,6 @@ export function getInput(commandPrompt: string, pauseSeconds?: number) {
     if (pauseSeconds) {
       timeout = setTimeout(() => {
         abortQuestion();
-        output.comment(`Wait expired. Continuing...`);
       }, pauseSeconds * 1000);
     }
 
@@ -102,7 +128,6 @@ export function getInput(commandPrompt: string, pauseSeconds?: number) {
           .then((unreadThreadIds) => {
             if (unreadThreadIds.length) {
               abortQuestion();
-              output.comment(`Wait broken by new mail notification.`);
             }
           })
           // Catch and log errors, but don't break the interval on hopefully an intermittent error
