562083c7553c0dc5a549d001ed3f51ed1b3252d4
John Marshall <34883+swax@users.noreply.github.com>
2026-03-25 22:52:48 -0700

    Revise compact markdown styling so there is a bit of spacing between paragraphs, and less between blockquotes

--- packages/common-browser/src/CompactMarkdown.tsx
+++ packages/common-browser/src/CompactMarkdown.tsx
@@ -3,7 +3,21 @@ import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
 const compactComponents: Components = {
-  p: ({ children }) => <p style={{ margin: 0 }}>{children}</p>,
+  p: ({ children }) => (
+    <p style={{ marginBlockStart: "0.5em", marginBlockEnd: "0.5em" }}>
+      {children}
+    </p>
+  ),
+  blockquote: ({ children }) => (
+    <blockquote
+      style={{
+        marginBlockStart: "0em",
+        marginBlockEnd: "0em",
+      }}
+    >
+      {children}
+    </blockquote>
+  ),
   ul: ({ children }) => (
     <ul style={{ margin: "0.25em 0", paddingLeft: "1.5em" }}>{children}</ul>
   ),
